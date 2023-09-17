4bf9de28576828b64754082a6f407c218ce09252
Caick da Silva Galvão <88976384+Caickolas@users.noreply.github.com>
2023-09-17 19:34:37 -0300

    Aula 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Login } from './views/Login.tsx'
+import { Login } from './views/Login'
 
-import './assets/styles/global.scss'
+import {RouterProvider} from 'react-router-dom'
+import {router} from './router'
+import './assets/styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Login />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
